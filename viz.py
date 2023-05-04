87a8cac532b913556be35c60f5bd9e380358a474
nengkya <nengkya@yahoo.com>
2023-05-04 23:21:35 +0700

    mesa error

--- viz.py
+++ viz.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 #rcparams = run configuration parameters
+
+df = pd.read_csv('thai/thai-export/export 2012 August 39011019090.csv')
+
+df.plot()
+
+plt.show()
